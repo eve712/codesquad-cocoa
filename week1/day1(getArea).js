9b0ca5d6c00cd76e5e76359a02b03074efd3040c
eve712 <62237639+eve712@users.noreply.github.com>
2020-11-03 13:00:01 +0900

    Update day1(getArea).js
    매개변수명 변경

--- week1/day1(getArea).js
+++ week1/day1(getArea).js
@@ -12,34 +12,34 @@ function printExecutionSequence() {
     }
 }
 
-function getArea(name, a, b, c) {
-    if (name === 'circle') {
-        if (a < b) {
-            getCircleSum(b);
+function getArea(shape, value1, value2, value3) {
+    if (shape === 'circle') {
+        if (value1 < value2) {
+            getCircleSum(value2);
         }
         else {
-            getCircle(name, a);
+            getCircle(value1);
         }
     }
-    else if (name === 'rect') {
-        console.log(a * b);
-        logExecution('rect', a * b);
+    else if (shape === 'rect') {
+        console.log(value1 * value2);
+        logExecution('rect', value1 * value2);
     }
-    else if (name === 'trapezoid') {
-        console.log((a + b) * c / 2);
-        logExecution('trapezoid', (a + b) * c / 2);
+    else if (shape === 'trapezoid') {
+        console.log((value1 + value2) * value3 / 2);
+        logExecution('trapezoid', (value1 + value2) * value3 / 2);
     }
 }
 
-function getCircle(name, a) {
-    console.log(a * a * 3.14);
-    logExecution('circle', a * a * 3.14);
+function getCircle(radius) {
+    console.log(radius * radius * 3.14);
+    logExecution('circle', radius * radius * 3.14);
 }
 
-function getCircleSum(b) {
+function getCircleSum(radius) {
     var result = [];
     var sum = 0;
-    for(var i = 0; i < b; i++) {
+    for(var i = 0; i < radius; i++) {
         result[i] = (i + 1) * (i + 1) * 3.14;
         sum += result[i];
     }
