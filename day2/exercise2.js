ea3dda76a2b7889a8a2fb77cb0987358825afbf2
eve712 <62237639+eve712@users.noreply.github.com>
2020-11-04 16:09:44 +0900

    filterId함수 만들기

--- day2/exercise2.js
+++ day2/exercise2.js
@@ -13,4 +13,33 @@ function calculate(n) {
     }
     return factorialArray;
 }
+
 console.log(calculate(4)); // 1, 2, 6, 24
+//----------------------------------------------
+
+const peoples = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
+
+function filterId(str) {
+    let result = [];
+    result = str.map(item => nonWordRemv(item)); 
+    while(result.indexOf(undefined) != -1) {
+        result.splice(result.indexOf(undefined), 1);
+    }
+    result = result.map(item => numberRemv(item));
+    return result;
+}
+function nonWordRemv (item) {
+    if (/\W/.test(item) === true) {
+        return;
+    }
+    else {
+        return item;
+    }
+}
+function numberRemv (item) {
+    item = item.replace(/[0-9]/g, '');
+    return item;
+}
+
+console.log(filterId(peoples));   // ['honux', 'head', 'zello', 'lucas']
+//----------------------------------------------
