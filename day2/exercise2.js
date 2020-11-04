cab54e6e472e2dd944669fef668fc45ee8900d05
eve712 <62237639+eve712@users.noreply.github.com>
2020-11-04 21:46:53 +0900

    averageGrade 추가

--- day2/exercise2.js
+++ day2/exercise2.js
@@ -21,14 +21,14 @@ const peoples = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
 
 function filterId(str) {
     let result = [];
-    result = str.map(item => nonWordRemv(item)); 
+    result = str.map(nonWordRemv); 
     while(result.indexOf(undefined) != -1) {
         result.splice(result.indexOf(undefined), 1);
     }
-    result = result.map(item => numberRemv(item));
+    result = result.map(numberRemv);
     return result;
 }
-function nonWordRemv (item) {
+function nonWordRemv(item) {
     if (/\W/.test(item) === true) {
         return;
     }
@@ -36,10 +36,18 @@ function nonWordRemv (item) {
         return item;
     }
 }
-function numberRemv (item) {
+function numberRemv(item) {
     item = item.replace(/[0-9]/g, '');
     return item;
 }
 
 console.log(filterId(peoples));   // ['honux', 'head', 'zello', 'lucas']
 //----------------------------------------------
+const grades = [[88,76,77], [33,44,44], [90,100,94], [30,44,98]];
+
+// 각 학생의 평균 점수 구하기
+let averageGrade = grades.map(item => {
+    const sum = item.reduce((a, b) => a + b);
+    return sum / item.length;
+});
+console.log(averageGrade);
