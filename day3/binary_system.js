045e56b1dbcf2ba71f3e277f31dd8e102925abdb
eve712 <62237639+eve712@users.noreply.github.com>
2020-11-05 12:58:17 +0900

    resultArray 배열 비우기, saveArr 객체 생성

--- day3/binary_system.js
+++ day3/binary_system.js
@@ -1,32 +1,33 @@
 
-// n진법 수로 바꾼 수들의 배열이 들어가 있는 배열 
-let resultArray = [];
+let saveArr = {
+    resultArray: [],
+    pthAnswer: []
+};
 
 // n진수, t개의 숫자까지, m명이 말하고, p번째
 function solution(n, t, m, p) {
+    saveArr.resultArray.length = 0;
     for(let i = 0; i < t * m; i++) {
-        let result = decimalToNSystem(i, n); // n진법 수 문자열로 반환
-        let arr = stringToArray(result); // 한 글자씩 쪼개서 배열로 만듦
-        let arr2 = resultArray.concat(arr); // 배열합치기
-        resultArray = arr2; 
+        let result = decimalToNSystem(i, n); 
+        let arr = stringToArray(result);  
+        let arr2 = saveArr.resultArray.concat(arr); 
+        saveArr.resultArray = arr2; 
     }
     if(p > 0) {
-       return getPthAnswer(m, p);
+        return getPthAnswer(m, p);
     }
     else {
-        return resultArray;
+        return saveArr.resultArray;
     }
 }
-console.log(solution(2, 4, 2));
-console.log(solution(2, 4, 2, 2));
 
 // p번째 차례의 답 구하는 함수
 function getPthAnswer(m, p) {
-    let pthAnswer = [];
-    for (let i = p - 1; i < resultArray.length; i += m) {
-        pthAnswer += resultArray[i];
+    saveArr.pthAnswer.length = 0;
+    for (let i = p - 1; i < saveArr.resultArray.length; i += m) {
+        saveArr.pthAnswer += saveArr.resultArray[i];
     }
-    return pthAnswer;
+    return saveArr.pthAnswer;
 }
 
 // 십진법 수를 n진수로 바꾸는 함수
@@ -41,3 +42,7 @@ function stringToArray(string) {
     return arr;
 }
 
+console.log(solution(2, 4, 2));
+console.log(solution(2, 4, 2, 2));
+console.log(solution(3, 5, 4));
+console.log(solution(16, 5, 3));
