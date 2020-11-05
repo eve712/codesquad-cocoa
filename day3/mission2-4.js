8f0821cbf1255ee40a3cc9259bae3d2e1d3a2a54
eve712 <62237639+eve712@users.noreply.github.com>
2020-11-05 18:49:16 +0900

    주석 삭제

--- day3/mission2-4.js
+++ day3/mission2-4.js
@@ -1,12 +1,10 @@
+
+
 const o = require('./o.js');
 let numberkey = [];
 
 function getNumberKey(obj) {
     for(var key in obj) {
-        // let dataValue = obj.key;
-        // key의 자료형은 string이기 때문에!! 
-        // obj.key는 o.data.'window'가 되기 때문에 접근법이 잘못돼서 undefined이다.
-        // 따라서, obj.key가 아니라 obj[key]로 접근해야 속성값에 접근할 수 있다.
         let dataValue = obj[key];
         if(typeof dataValue === "number") {
             numberkey.push(key);
@@ -18,3 +16,5 @@ function getNumberKey(obj) {
     return numberkey;
 }
 console.log(getNumberKey(o.data));
+
+
