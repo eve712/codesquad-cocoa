6962621ef597b84f4ff6e5396d282f0230c2a713
eve712 <62237639+eve712@users.noreply.github.com>
2020-11-06 15:47:39 +0900

    myReduce 동작 test2 추가

--- day3/mission2-6.js
+++ day3/mission2-6.js
@@ -1,6 +1,4 @@
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const reducer = (a, b) => a + b;
 
 const myReduce = (arr, callback, initialValue) => {
     let accumulator = callback(initialValue, arr[0]);
@@ -10,11 +8,19 @@ const myReduce = (arr, callback, initialValue) => {
     return accumulator;
 }
 
+// test1
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const reducer = (a, b) => a + b;
+
 let result = myReduce(arr, reducer, 10);
 console.log(result);
 console.log(arr.reduce(reducer, 10));  //같은 결과
 
-
-
-
-//const result = myReduce(arr, (next, prev) => {...}, []);
+// test2
+const arr2 = [{x: 1}, {x: 2}, {x: 3}];
+const reducer2 = (accumulator, currentValue) => {
+    return accumulator + currentValue.x;
+}
+result = myReduce(arr2, reducer2, 0);
+console.log(result);
+console.log(arr2.reduce(reducer2, 0)); //같은 결과
