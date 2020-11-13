3c67852bf5d7289dafb92276465f33c2a95be606
eve712 <62237639+eve712@users.noreply.github.com>
2020-11-13 15:33:46 +0900

    다른 풀이 분석(배열 중복 제거 Set으로)

--- algorithm/pr68644.js
+++ algorithm/pr68644.js
@@ -1,20 +1,33 @@
 function solution(numbers) {
-    let answer = [];
-    let sum = 0;
     let sumArr = [];
     for(let i = 0; i < numbers.length; i++) {
         for(let j = 0; j < numbers.length; j++) {
             if (i !== j) {
-                sum = numbers[i] + numbers[j];
-                sumArr.push(sum);
+                sumArr.push(numbers[i] + numbers[j]);
             }
         }
     }
-    answer = sumArr.reduce((acc,curr) => acc.includes(curr) ? acc : [...acc,curr],[]);
-    answer.sort((a, b) => a - b);
-    return answer;
+    const answer = sumArr.reduce(
+        (acc,curr) => acc.includes(curr) ? acc : [...acc,curr],
+        []
+    );
+    return answer.sort((a, b) => a - b);
+}
+
+//----------다른 풀이-----------
+// 📌 배열의 중복 제거 → Set
+function solution(numbers) {
+    const temp = []
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            temp.push(numbers[i] + numbers[j])
+        }
+    }
+    const answer = [...new Set(temp)]
+    return answer.sort((a, b) => a - b)
 }
 
 
+//-----test-----
 numbers = [2,1,3,4,1];
 console.log(solution(numbers)); //[2,3,4,5,6,7]
