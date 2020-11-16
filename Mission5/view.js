0b8919418323cd5b0362b2f334b775a5906a0293
eve712 <62237639+eve712@users.noreply.github.com>
2020-11-17 01:44:38 +0900

    요소 삭제 구현 완료
    Event Delegation 이벤트 위임

--- Mission5/view.js
+++ Mission5/view.js
@@ -4,9 +4,7 @@ class View {
         this.inputText = document.getElementById('inputText');
         this.addButton = document.getElementById('addButton');
         this.list = document.getElementById('list');
-        this.listItems = document.querySelectorAll('#list > li');
-        this.checkboxes = document.getElementsByClassName('check');
-        this.removebuttons = document.getElementsByClassName('remove');
+        this.listLabels = document.getElementsByClassName('label');
     }
     createListItem (value) {
         const template = document.getElementById('template');
@@ -18,12 +16,11 @@ class View {
     addList() {
         let value = this.inputText.value;
         this.createListItem(value);
-        this.listItems = document.querySelectorAll('#list > li');
     }
     makeLineThrough() {
-        for(let item of view.listItems) {
-            let checkbox = item.firstElementChild.firstElementChild;
-            if(checkbox.checked) {
+        for(let item of view.listLabels) {
+            let checkbox = item.firstElementChild;
+            if(!checkbox.checked) {
                 checkbox.nextElementSibling.style.textDecoration = "line-through";
             }
             else {
@@ -32,10 +29,24 @@ class View {
         }
     }
 }
-
-// test
 let view = new View();
 
+// 1. add 버튼 클릭 → 리스트 추가
 view.addButton.addEventListener("click", view.addList.bind(view));
 
-view.list.addEventListener("click", view.makeLineThrough.bind(view));
+// 2. label 클릭 → 체크상태따라 취소선 / i 클릭 → 요소 삭제
+view.list.addEventListener("click", function(e) {
+    if(e.target.nodeName === 'I') {
+        e.target.parentElement.parentElement.remove();
+    }
+    if(e.target.nodeName === 'LABEL') {
+        view.makeLineThrough();
+    }
+});
+
+
+
+
+
+// view.list.addEventListener("click", view.makeLineThrough.bind(view));
+// 이렇게 했을 때는 (checkbox.checked)로 해야 됨.
