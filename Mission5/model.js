d3d9073c147154077870d5fc4daed43e07c17598
eve712 <62237639+eve712@users.noreply.github.com>
2020-11-16 17:13:23 +0900

    취소선 진행중

--- Mission5/model.js
+++ Mission5/model.js
@@ -4,6 +4,7 @@ class Model {
         this.inputText = document.getElementById('inputText');
         this.addButton = document.getElementById('addButton');
         this.list = document.getElementById('list');
+        this.listItems = document.querySelectorAll('#list > li');
         this.checkboxes = document.getElementsByClassName('check');
         this.removebuttons = document.getElementsByClassName('remove');
     }
@@ -11,7 +12,7 @@ class Model {
         const template = document.getElementById('template');
         const copy = template.content.cloneNode(true);
         const node = document.createTextNode(value);
-        copy.querySelector('label').appendChild(node);
+        copy.querySelector('span').appendChild(node);
         this.list.appendChild(copy);
     }
     addList() {
@@ -22,4 +23,20 @@ class Model {
 
 // test
 let model = new Model();
+
 model.addButton.addEventListener("click", model.addList);
+
+for(let listItem of model.listItems) {
+    console.log(listItem);
+}
+
+/*
+for(let listItem of model.listItems) {
+    listItem.addEventListener("click", function() {
+        const checkbox = this.firstElementChild.firstElementChild;
+        if(checkbox.checked) {
+            checkbox.nextElementSibling.style.textDecoration = "line-through";
+        }
+    });
+}
+*/
