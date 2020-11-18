66b63ab8b558489a855d2525558acc3d6f6a558a
eve712 <62237639+eve712@users.noreply.github.com>
2020-11-18 17:53:57 +0900

    리팩토링, 주석정리

--- Mission6/todolist.js
+++ Mission6/todolist.js
@@ -3,7 +3,6 @@ class TodoModel {
         this.modelArray = [];
         this.countId = 1;
     }
-    // 1. input 입력값을 받아서 배열에 추가
     addToArray(task) {
         const modelObj = {
             elementId: this.countId++,
@@ -15,7 +14,6 @@ class TodoModel {
         }
         return modelObj;
     }
-    // 2. checked 상태 업데이트
     updateChecked(id) {
         const targetIndex = this.getTargetIndex(id);
         const targetEl = this.modelArray[targetIndex];
@@ -25,7 +23,6 @@ class TodoModel {
             targetEl.checked = false;
         }
     }
-    // 3. remove 발생하면 배열 업데이트
     updateRemove(id) {
         this.modelArray.splice(this.getTargetIndex(id), 1);
     }
@@ -41,7 +38,6 @@ class TodoView {
         this.listWrapper = reference.listWrapper;
         this.todoModel = reference.todoModel;
     }
-    // 0. 이벤트 리스너 작성
     initEvent(todoViewObj) {
         this.addButton.addEventListener("click", this.addList.bind(this));
         this.listWrapper.addEventListener("click", function({target}) {
@@ -49,7 +45,6 @@ class TodoView {
             if(target.nodeName === 'I') { todoViewObj.removeListItem(target); }
         });
     }
-    // 1. add버튼 누르면 list 추가
     addList() {
         const task = this.inputEl.value;
         const modelObj = this.todoModel.addToArray(task);
@@ -66,7 +61,6 @@ class TodoView {
         copy.querySelector('li').setAttribute('id', elementId);
         this.listWrapper.appendChild(copy);
     }
-    // 2. 체크하면 취소선
     makeLineThrough(target) {
         const checkbox = target.firstElementChild;
         if(!checkbox.checked) {
@@ -78,7 +72,6 @@ class TodoView {
         const elementId = target.parentElement.id;
         this.todoModel.updateChecked(elementId);
     }
-    // 3. 휴지통 누르면 List 삭제
     removeListItem(target) {
         const removeEl = target.parentElement.parentElement;
         const removeElId = removeEl.id;
