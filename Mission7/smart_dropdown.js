9592852bccb5e1e74caf5bed4c8a2bc393517dec
eve712 <62237639+eve712@users.noreply.github.com>
2020-11-23 17:44:06 +0900

    리팩토링: class 사용, DOM 참조 객체로 묶기
    * 함수들을 ShowingList, CountingList 클래스로 나누어 분류
    
    * DOM 요소 참조한 것들을 reference 객체에 담아서 class에 인자로 전달

--- Mission7/smart_dropdown.js
+++ Mission7/smart_dropdown.js
@@ -1,66 +1,81 @@
-const titleOfList = document.getElementById('titleOfList');
-const fruitList = document.getElementById('fruitList');
-const listItem = document.querySelectorAll('#fruitList > li');
-const container = document.getElementById('container');
-let timerOfTitle;
-let timerOfList;
+
+//----●● 1초 이상 머무르면 list 보여주기 ●●--------------------
+class ShowingList {
+    constructor({titleOfList, container, fruitList}) {
+        this.titleOfList = titleOfList;
+        this.container = container;
+        this.fruitList = fruitList;
+        this.timerOfTitle = null;
+    }
+    initEvent() {
+        this.titleOfList.addEventListener('mouseenter', this.setTimer.bind(this));
+        this.titleOfList.addEventListener('mouseleave', this.clearTimer.bind(this));
+        this.container.addEventListener('mouseleave', this.hideList.bind(this));
+    }
+    showList() {
+        this.fruitList.classList.remove('hiddenList');
+        this.fruitList.classList.add('shownList');
+    }
+    hideList() {
+        this.fruitList.classList.remove('shownList');
+        this.fruitList.classList.add('hiddenList');        
+    }
+    // 타이머 설정, 타이머ID 할당
+    setTimer() {
+        this.timerOfTitle = setTimeout(this.showList.bind(this), 1000);    
+    }
+    // 타이머 해제
+    clearTimer() {
+        clearTimeout(this.timerOfTitle);    
+    }
+}
+
+
+//----●● 마우스 이동정보 출력 ●●--------------------
+class CountingList {
+    constructor({listItem}) {
+        this.listItem = listItem;
+        this.timerOfList = null;
+    }
+    initEvent() {
+        this.listItem.forEach(el => el.addEventListener('mouseenter', this.setPrinter.bind(this)));
+        this.listItem.forEach(el => el.addEventListener('mouseleave', this.clearPrinter.bind(this)));
+    }
+    createPrintingEl(target) {
+        const box = document.getElementById('counting');
+        let el = document.createElement('div');
+        el.id = target.innerText;
+        el.innerText = `${target.innerText} : 1`;
+        box.appendChild(el);
+    }    
+    // 매개변수 el = div로 새로 만들어진 요소 (printedEl)
+    count(el) {
+        let textArr = el.innerText.split(' : ');
+        let count = textArr[1];
+        el.innerText = `${el.id} : ${++count}`;
+    }
+    // 매개변수 target = mouseenter 이벤트된 li 요소
+    setPrinter({target}) {
+        let printingEl = document.getElementById(target.innerText);
+        if (!printingEl) this.createPrintingEl(target);
+        let printedEl = document.getElementById(target.innerText);
+        this.timerOfList = setInterval(this.count.bind(null, printedEl), 500);    
+    }
+    clearPrinter() {
+        clearInterval(this.timerOfList);
+    }
+}
 
 //----------------------------------------------------
 
-const showList = function() {
-    fruitList.classList.remove('hiddenList');
-    fruitList.classList.add('shownList');
-};
+const reference = {
+    titleOfList: document.getElementById('titleOfList'),
+    container: document.getElementById('container'),
+    fruitList: document.getElementById('fruitList'),
+    listItem:  document.querySelectorAll('#fruitList > li')
+}
 
-const hideList = function() {
-    fruitList.classList.remove('shownList');
-    fruitList.classList.add('hiddenList');
-};
-
-// 타이머 설정, 타이머ID 할당
-const setTimer = function() {
-    timerOfTitle = setTimeout(showList, 1000);
-};
-
-// 타이머 해제
-const clearTimer = function() {
-    clearTimeout(timerOfTitle);
-};
-
-//----------------------------------------------------
-
-const createPrintingEl = function(target) {
-    let el = document.createElement('div');
-    el.id = target.innerText;
-    el.innerText = `${target.innerText} : 1`;
-    document.body.appendChild(el);
-};
-
-// 매개변수 el = div로 새로 만들어진 요소 (printedEl)
-const count = function(el) {
-    console.log(el);
-    let textArr = el.innerText.split(' : ');
-    let count = textArr[1];
-    el.innerText = `${el.id} : ${++count}`;
-};
-
-// 매개변수 target = mouseenter 이벤트된 li 요소
-const setPrinter = function({target}) {
-    let printingEl = document.getElementById(target.innerText);
-    if (!printingEl) createPrintingEl(target);
-    printedEl = document.getElementById(target.innerText);
-    timerOfList = setInterval(count.bind(null, printedEl), 500);
-};
-
-const clearPrinter = function() {
-    clearInterval(timerOfList);
-};
-
-//----------------------------------------------------
-
-titleOfList.addEventListener('mouseenter', setTimer);
-titleOfList.addEventListener('mouseleave', clearTimer);
-container.addEventListener('mouseleave', hideList);
-listItem.forEach(el => el.addEventListener('mouseenter', setPrinter));
-listItem.forEach(el => el.addEventListener('mouseleave', clearPrinter));
-
+let showingList = new ShowingList(reference);
+showingList.initEvent();
+let countingList = new CountingList(reference);
+countingList.initEvent();
