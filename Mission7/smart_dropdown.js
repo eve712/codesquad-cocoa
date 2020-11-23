488f4323e2bfde66472d81a375d5d54c93c62453
eve712 <62237639+eve712@users.noreply.github.com>
2020-11-23 15:54:47 +0900

    기능추가: li요소 마우스이벤트 따라 count 기능
    * 모든 li요소에 이벤트리스너 연결(mouseenter, mouseleave)
    
    * setPrinter → mouseenter이벤트의 핸들러 함수(요소 추가, 타이머에 맞춰서 카운트 증가)
    
    *clearPrinter → mouseleave이벤트의 핸들러 함수(setInterval 함수 해제)
    
    * createPrintingEl → 카운트해주는 요소 문서에 추가해주는 함수
    
    * count → setInterval의 핸들러로 count 증가

--- Mission7/smart_dropdown.js
+++ Mission7/smart_dropdown.js
@@ -1,7 +1,11 @@
 const titleOfList = document.getElementById('titleOfList');
 const fruitList = document.getElementById('fruitList');
+const listItem = document.querySelectorAll('#fruitList > li');
 const container = document.getElementById('container');
-let timerId;
+let timerOfTitle;
+let timerOfList;
+
+//----------------------------------------------------
 
 const showList = function() {
     fruitList.classList.remove('hiddenList');
@@ -11,17 +15,52 @@ const showList = function() {
 const hideList = function() {
     fruitList.classList.remove('shownList');
     fruitList.classList.add('hiddenList');
-}
+};
 
 // 타이머 설정, 타이머ID 할당
 const setTimer = function() {
-    timerId = setTimeout(showList, 2000);
-}
+    timerOfTitle = setTimeout(showList, 1000);
+};
+
 // 타이머 해제
 const clearTimer = function() {
-    clearTimeout(timerId);
-}
+    clearTimeout(timerOfTitle);
+};
+
+//----------------------------------------------------
+
+const createPrintingEl = function(target) {
+    let el = document.createElement('div');
+    el.id = target.innerText;
+    el.innerText = `${target.innerText} : 1`;
+    document.body.appendChild(el);
+};
+
+// 매개변수 el = div로 새로 만들어진 요소 (printedEl)
+const count = function(el) {
+    console.log(el);
+    let textArr = el.innerText.split(' : ');
+    let count = textArr[1];
+    el.innerText = `${el.id} : ${++count}`;
+};
+
+// 매개변수 target = mouseenter 이벤트된 li 요소
+const setPrinter = function({target}) {
+    let printingEl = document.getElementById(target.innerText);
+    if (!printingEl) createPrintingEl(target);
+    printedEl = document.getElementById(target.innerText);
+    timerOfList = setInterval(count.bind(null, printedEl), 500);
+};
+
+const clearPrinter = function() {
+    clearInterval(timerOfList);
+};
+
+//----------------------------------------------------
 
 titleOfList.addEventListener('mouseenter', setTimer);
 titleOfList.addEventListener('mouseleave', clearTimer);
 container.addEventListener('mouseleave', hideList);
+listItem.forEach(el => el.addEventListener('mouseenter', setPrinter));
+listItem.forEach(el => el.addEventListener('mouseleave', clearPrinter));
+
