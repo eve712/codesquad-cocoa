27fd2cffda1c07a81598f41413f2f9167b6a4421
eve712 <62237639+eve712@users.noreply.github.com>
2020-11-24 13:45:05 +0900

    리팩토링: forEach 중복 하나의 함수로 만듦

--- Mission7/smart_dropdown.js
+++ Mission7/smart_dropdown.js
@@ -1,4 +1,3 @@
-
 //----●● 1초 이상 머무르면 list 보여주기 ●●--------------------
 class ShowingList {
     constructor({titleOfList, container, fruitList}) {
@@ -37,8 +36,11 @@ class CountingList {
         this.timerOfPrinting;
     }
     initEvent() {
-        this.listItem.forEach(el => el.addEventListener('mouseenter', this.createPrintingEl.bind(this)) );
-        setInterval(this.setEvent.bind(this), 500);
+        this.setEvent('mouseenter', this.createPrintingEl);
+        setInterval(this.setEvent.bind(this, 'mousemove', this.count, {once: true}), 500);
+    }
+    setEvent(type, func, option = null) {
+        this.listItem.forEach(el => el.addEventListener(type, func.bind(this), option));
     }
     // listItem에 첫 mouseenter 이벤트가 발생할 때 counting해주는 새로운 요소 생성
     createPrintingEl({target}) {
@@ -50,11 +52,7 @@ class CountingList {
             this.wrapper.appendChild(el);
         };
     }
-    setEvent() {
-        this.listItem.forEach(el => el.addEventListener('mousemove', this.count.bind(this), {once: true}));
-    }
-    // 카운팅하는 요소가 문서에 있으면 setInterval로 계속 카운팅
-    // 매개변수 target = mouseenter 발생한 li 요소
+    // counting해주는 함수
     count({target}) {
         let el = document.getElementById(target.innerText);
         let textArr = el.innerText.split(' : ');
