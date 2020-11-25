a050e4bc972e1055018e3c34f72054e24cbaaf95
eve712 <62237639+eve712@users.noreply.github.com>
2020-11-25 17:24:41 +0900

    기능추가: 마을node를 만드는 함수 추가
    * createName은 마을의 이름 랜덤으로 정하는 함수
    
    * createPostbox는 우편함이 있는지 여부 정하는 함수
    
    * createTown은 위 두 함수를 호춣해서 name, postbox를 인자로 전달해 새로우 객체 생성. 마을의 개수만큼 반복한다.

--- Mission8/postbox.js
+++ Mission8/postbox.js
@@ -1,8 +1,12 @@
 
-// 1. 마을의 개수
-// 2. child를 가질 마을 개수
-// 3. child를 가질 마을을 선택(idx), 마을의 개수(2)만큼 반복
-// 4. townRelation[idx]의 child에 새로운 노드를 생성해서 push
+//  마을의 개수
+//  마을의 개수만큼 CreatNode로 만듦.
+//  name > random()으로 52개 알파벳 중 하나 랜덤 지정.
+//  postbox > random()으로 확률 지정해서 bool값 반환하도록.
+
+//  child를 가질 마을 개수
+//  child를 가질 마을을 선택(idx), 마을의 개수(2)만큼 반복
+//  townRelation[idx]의 child에 새로운 노드를 생성해서 push
 
 // ----------------------------------------
 
@@ -18,24 +22,39 @@ class TownModel {
     constructor() {
         this.townRelation = [];
         this.indexes = [];
-        this.numOfFirst;
+        this.numOfTown;
         this.numOfHavingChild;
     }
-    // 마을을 새로운 노드로 만드는 함수
-    createTown () {
-        // name, value값을 정해줘야함
-        let newTown = new CreateNode();
-    }
-    // 1~max까지 랜덤으로 정수 반환
+    // min ~ max까지 랜덤으로 정수 반환
     getRandomNum (min, max) {
         const random = Math.floor(Math.random() * max) + min;
         return random;
     }
-
-    // 1차 마을의 개수, child를 가질 마을의 개수
-    decideNum() {
-        this.numOfFirst = this.getRandomNum(1, 5);
-        this.numOfHavingChild = this.getRandomNum(0, 5);
+    // 마을의 이름 랜덤으로 정하는 함수
+    createName () {
+        const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const name = alphabet.charAt(this.getRandomNum(0, alphabet.length - 1));
+        return name;
+    }
+    // 30% postbox true
+    createPostbox() {
+        let num = Math.random();
+        if(num >= 0.7) return true;
+        else return false;
+    }
+    // 마을을 새로운 노드로 만드는 함수
+    createTown (n) {
+        for(let i = 0; i < n; i++) {
+            const name = this.createName();
+            const postbox = this.createPostbox();
+            let newTown = new CreateNode(name, postbox);
+            this.townRelation.push(newTown);
+        }
+    }
+    // 마을의 개수, child를 가질 마을의 개수
+    decideNum(max) {
+        this.numOfTown = this.getRandomNum(1, max);
+        this.numOfHavingChild = this.getRandomNum(0, max);
     }
     // chlid를 가질 마을 선택(idx 정하기)
     // n = this.numOfHavingChild 뽑아야할 idx 개수
