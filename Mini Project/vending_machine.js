b95e2e63ae7440399b40e641614e17434a0c843e
eve712 <62237639+eve712@users.noreply.github.com>
2020-12-01 15:14:34 +0900

    기능추가: wallet data의 초기값을 만드는 클래스 작성

--- Mini Project/vending_machine.js
+++ Mini Project/vending_machine.js
@@ -2,7 +2,7 @@
 // 메뉴데이터 생성자
 const DataTemplate = function(el, idx, obj) {
     this.number = idx + 1;
-    this.name = obj.createName(el);
+    this.name = obj.getName(el);
     this.price = el.lastElementChild.innerText;
 }
 
@@ -12,7 +12,7 @@ class MenuData {
         this.menuElArr = obj.menu; 
         this.menuDataArr = [];
     }
-    createName(el) {
+    getName(el) {
         const numberName = el.firstElementChild.nextElementSibling.innerText;
         const lastIdx = 1;
         const name = numberName.split('. ')[lastIdx];
@@ -24,6 +24,36 @@ class MenuData {
     }
 }
 
+// ------------● Wallet Data ●--------------
+// 사이트가 로드될 때 지갑의 초기값을 설정해주는 클래스. (이후 변동은 view클래스에서 적용)
+class WalletData {
+    constructor() {
+        this.coin100;
+        this.coin500;
+        this.paper1000;
+        this.paper5000;
+        this.paper10000;
+        this.moneyNumArr = [];
+    }
+    getRandomNum (min, max) {
+        const random = Math.floor(Math.random() * (max - min + 1)) + min;
+        return random;
+    }
+    assignNum (min1, max1, min2, max2) {
+        this.coin100 = this.getRandomNum(min1, max1);
+        this.paper1000 = this.getRandomNum(min1, max1);
+        this.coin500 = this.getRandomNum(min2, max2);
+        this.paper5000 = this.getRandomNum(min2, max2);
+        this.paper10000 = this.getRandomNum(min2, max2);
+    }
+    assignNumArr() {
+        this.moneyNumArr = [this.coin100, this.coin500, this.paper1000, this.paper5000, this.paper10000];
+    }
+    initMoneyNum(min1, max1, min2, max2) {
+        this.assignNum(min1, max1, min2, max2);
+        this.assignNumArr();
+    }
+}
 
 // ------DOM 참조------
 const reference = {
@@ -35,3 +65,7 @@ const reference = {
 const menuData = new MenuData(reference);
 menuData.createData();  // menuDataArr 생성
 console.log(menuData.menuDataArr);
+
+const walletData = new WalletData();
+walletData.initMoneyNum(4, 9, 2, 5); // 지갑의 초기값 설정
+console.log(walletData.moneyNumArr);
