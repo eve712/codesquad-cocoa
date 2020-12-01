20c9fefdc3bbb5de53619fcdce34a8dfc20c6676
eve712 <62237639+eve712@users.noreply.github.com>
2020-12-01 15:33:02 +0900

    기능추가: wallet data에서 금액 합계 구하는 함수 작성

--- Mini Project/vending_machine.js
+++ Mini Project/vending_machine.js
@@ -34,11 +34,13 @@ class WalletData {
         this.paper5000;
         this.paper10000;
         this.moneyNumArr = [];
+        this.total;
     }
     getRandomNum (min, max) {
         const random = Math.floor(Math.random() * (max - min + 1)) + min;
         return random;
     }
+    // 동전, 지폐 개수 랜덤 뽑기
     assignNum (min1, max1, min2, max2) {
         this.coin100 = this.getRandomNum(min1, max1);
         this.paper1000 = this.getRandomNum(min1, max1);
@@ -46,12 +48,21 @@ class WalletData {
         this.paper5000 = this.getRandomNum(min2, max2);
         this.paper10000 = this.getRandomNum(min2, max2);
     }
+    // 동전, 지폐 개수 배열
     assignNumArr() {
         this.moneyNumArr = [this.coin100, this.coin500, this.paper1000, this.paper5000, this.paper10000];
     }
+    // 총 금액의 합 구하는 함수
+    sumAmount() {
+        const value = [100, 500, 1000, 5000, 10000];
+        const num = this.moneyNumArr;
+        this.total = value.reduce((acc, curr, i) => acc + (curr * num[i]), 0);
+    }
+    // 개수, 배열, 총합 구하는 함수
     initMoneyNum(min1, max1, min2, max2) {
         this.assignNum(min1, max1, min2, max2);
         this.assignNumArr();
+        this.sumAmount();
     }
 }
 
@@ -59,7 +70,6 @@ class WalletData {
 const reference = {
     menu: document.getElementsByClassName('menu')
 }
-
 
 // ------ test ------
 const menuData = new MenuData(reference);
@@ -69,3 +79,4 @@ console.log(menuData.menuDataArr);
 const walletData = new WalletData();
 walletData.initMoneyNum(4, 9, 2, 5); // 지갑의 초기값 설정
 console.log(walletData.moneyNumArr);
+console.log(walletData.total);
